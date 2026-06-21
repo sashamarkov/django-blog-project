8dc746015dac9923fe3a33b789af495d06842680
Sasha Markov <sasha.markov.mail@gmail.com>
2026-06-21 17:38:08 +0300

    ДЗ2: Добавлены CSS стили, страница Контакты с формой

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Article
+import datetime
 
 def index(request):
     articles = Article.objects.all()
     return render(request, 'blog/index.html', {'articles': articles})
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        
+        with open('messages.txt', 'a', encoding='utf-8') as f:
+            f.write(f'--- {datetime.datetime.now()} ---\n')
+            f.write(f'Имя: {name}\n')
+            f.write(f'Email: {email}\n')
+            f.write(f'Сообщение: {message}\n')
+            f.write('\n')
+        
+        return HttpResponse('Спасибо! Ваше сообщение отправлено.')
+    
+    return render(request, 'blog/contacts.html')
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -82,3 +82,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
