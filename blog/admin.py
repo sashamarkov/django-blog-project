92203cc83eb00e3d0f73f2f0e5ca76d344b4cd12
Sasha Markov <sasha.markov.mail@gmail.com>
2026-06-21 18:00:45 +0300

    ДЗ5: Настройка админки для управления статьями

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Article, ContactMessage
 
-admin.site.register(Article)
-admin.site.register(ContactMessage)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'short_description', 'created_at')
+    search_fields = ('title', 'short_description')
+    list_filter = ('created_at',)
+    fields = ('title', 'short_description', 'full_description', 'image')
+
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+    search_fields = ('name', 'email', 'message')
+    list_filter = ('created_at',)
+    readonly_fields = ('created_at',)
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(ContactMessage, ContactMessageAdmin)
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = 'Блог программиста - Администрирование'
+admin.site.site_title = 'Блог программиста'
+admin.site.index_title = 'Управление контентом'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
