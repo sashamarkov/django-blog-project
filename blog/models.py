c861ae963f760b197ee31de2528b0553636cefe7
Sasha Markov <sasha.markov.mail@gmail.com>
2026-06-21 17:32:54 +0300

    ДЗ1: Базовая структура блога

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    short_description = models.TextField(verbose_name='Краткое описание')
+    full_description = models.TextField(verbose_name='Полное описание')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
